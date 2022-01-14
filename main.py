80edfb739bb25c64df964661d80a43b26feb6ffa
HARUN PEHLİVAN <trcmnhp@hotmail.com>
2022-01-14 01:43:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+fig4, (ax, ax2) = plt.subplots(ncols=2, constrained_layout=True)
+
+np.random.seed(19680801)  # Fixing random state for reproducibility
+im = np.random.rand(16, 27)
+ax.imshow(im)
+
+ax2.plot([23, 45])
+ax2.set_box_aspect(im.shape[0]/im.shape[1])
+
+plt.show()
